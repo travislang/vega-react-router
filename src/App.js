8a0333f1f33062b4cbb1b139477e767c3a5be032
Travis Lang <tlang50@icloud.com>
2018-12-03 16:20:03 -0600

    add react-router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import { HashRouter as Router, Route } from 'react-router-dom';
+
 import Animals from './Components/Animals/Animals.js'
 import Plants from './Components/Plants/Plants.js'
 import Home from './Components/Home/Home.js'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-       
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                {/* <div className="App">
+                    <Home />
+                    <Plants />
+                    <Animals />
+                </div> */}
+                <div>
+                    <Route path='/' exact component={Home} />
+                    <Route path='/plants' component={Plants} />
+                    <Route path='/Animals' component={Animals} />
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
