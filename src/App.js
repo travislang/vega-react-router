d92833e5f85ef621c44cde12b6db015696fec3ab
Travis Lang <tlang50@icloud.com>
2018-12-03 15:56:29 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Animals from './Components/Animals/Animals.js'
+import Plants from './Components/Plants/Plants.js'
+import Home from './Components/Home/Home.js'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/Plants/Plants.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Plants extends Component {
+  render() {
+    return (
+      <div>
+        <h1>PLANTS</h1>
+      </div>
+    )
+  }
+}
+
+export default Plants;
